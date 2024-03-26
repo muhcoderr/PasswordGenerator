f5d0f955354eba486cabeb0c2d59a537ec5c4af5
muhcoderr <muhammad4948@gmail.com>
2024-03-26 19:33:38 +0500

    createPassword

--- App.tsx
+++ App.tsx
@@ -21,15 +21,20 @@ export default function App() {
 
   const generatePasswordString = (passwordLength: number) => {
     //
-  }
+  };
 
   const createPassword = (characters: string, passwordLength: number) => {
-    //
-  }
+    let result = "";
+    for (let i = 0; i < passwordLength; i++) {
+      const characterIndex = Math.round(Math.random() * characters.length);
+      result += characters.charAt(characterIndex);
+    }
+    return result;
+  };
 
   const resetPasswordState = () => {
     //
-  }
+  };
 
   return (
     <View>
